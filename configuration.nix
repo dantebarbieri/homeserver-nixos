aaacb5dbbeae1303ae73661c1b5a8db413faa999
dantebarbieri <dantevbarbieri@gmail.com>
2025-07-28 17:32:26 -0500

    Add zoxide

--- configuration.nix
+++ configuration.nix
@@ -158,6 +158,7 @@
   environment.systemPackages = with pkgs; [
     neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
+    zoxide
 
     # Storage tooling
     mdadm
